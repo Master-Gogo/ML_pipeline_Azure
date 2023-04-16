84ee2f66029264ec02a28b4fd823f57245cdad00
Ganesh Lahamge <ganeshlahamge0212@gmail.com>
2023-04-17 00:03:17 +0530

    final

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ml_azure",
+    version="0.0.1",
+    author="ganesh",
+    author_email="ganeshlahamge0212@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
